0a92c2c80c97211fb3ed3bb8386df7f5797ef393
yedohkang913 <ykang@stuy.edu>
2017-11-29 00:19:27 -0500

    worked on parsing, and added new functions that need to be worked on tomorrow

--- shell.c
+++ shell.c
@@ -6,100 +6,116 @@
 #include "shell.h"
 
 char** parse_args(char* line){
-  char** args = (char**)calloc(6, sizeof(char*));
-  int i = 0;
-  char * tp;
-  while(line){
-    tp = strsep(&line, " ");
-    args[i] = tp;
+	char** args = (char**)calloc(6, sizeof(char*));
+	int i = 0;
+	char * tp;
+	while(line){
+		tp = strsep(&line, " ");
+		args[i] = tp;
 
-    //checks for new line
-    tp = strsep(&tp, "\n");
-    if(!tp){
-      args[i] = 0;
-    }
-    i++;
-  }
-  return args;
+		//checks for new line
+		tp = strsep(&tp, "\n");
+		if(!tp){
+			args[i] = 0;
+		}
+		i++;
+	}
+	return args;
 }
 
 int execute_commands(char ** args){
-  //if the args are separated by ;
-  /*
-    char *** sep_args;
-    int args_i = 0;
-    int sep_args_i = 0;
-    while (args[args_i]) {
-    if(args[args_i] == ";"){
-    sep_args[sep_args_i] = args[0]; //args[0] to args[i]
-    sep_args_i++;
-    }
-    args_i++;
-    }
-  */
-  int prev = 0;
-  int curr = 0;
-  while(args[curr]){
-    if(strcmp(args[curr], ";") == 0){
-      char ** this_arg;
-      int i = 0;
-      while(curr != i + prev){ //????
-	this_arg[i] = args[prev + i];
-	i++;
-      }
+	//if the args are separated by ;
+	/*
+	char *** sep_args;
+	int args_i = 0;
+	int sep_args_i = 0;
+	while (args[args_i]) {
+	if(args[args_i] == ";"){
+	sep_args[sep_args_i] = args[0]; //args[0] to args[i]
+	sep_args_i++;
+	}
+	args_i++;
+	}
+	*/
+	int prev = 0;
+	int curr = 0;
+	while(args[curr]){
+		if(strcmp(args[curr], ";") == 0){
+			char ** this_arg;
+			int i = 0;
+			while(curr != i + prev){ //????
+				this_arg[i] = args[prev + i];
+				i++;
+			}
       
-      if(strcmp(args[0], "cd") == 0){
-	return 1;
-      }
-      if (execvp(args[0], args) == -1){
-	printf("%s: command not found\n", args[0]);
-	return -1;
-      }
-      prev = curr + 1;
-    }
-    curr++;
+			if(strcmp(args[0], "cd") == 0){
+				return 1;
+			}
+			if (execvp(args[0], args) == -1){
+				printf("%s: command not found\n", args[0]);
+				return -1;
+			}
+			prev = curr + 1;
+		}
+		curr++;
     
-  }
+	}
+	execvp(args[0], args);
 }
 
 void print_args(char ** args){
-  int i = 0;
-  while(args[i]){
-    printf("%s\n", args[i]);
-    i++;
-  }
+	int i = 0;
+	while(args[i]){
+		printf("%s\n", args[i]);
+		i++;
+	}
+}
+
+void parse_pipe(char *command) {
+	
+}
+
+void parse_redir(char *redir) {
+	
 }
 
 int parse_params() {
-  char line[256];
-  fgets(line, 256, stdin);
+	char line[256];
+	fgets(line, 256, stdin);
+	char *command[256];
 
-  int f = fork();
-  char** args = parse_args(line);
-  //if child
-  if(f == 0){
-    return execute_commands(args);
-  }
-  //if parent
-  else{
-    int status;
-    wait(&status);
-    //cd
-    if(status == 1){
-      if(chdir(args[1]) != 1){
-	printf("Current directory is now: %s\n", args[1]);
-      }
-      else{
-	printf("%s: directory not found\n", args[1]);
-      }
-    }
-    
-  }
+	int i;
+	for (i = 0; command[i] = strsep(line, ";"); i++) {
+		
+		// command[i] = something where we move the pointer to the first non-whitespace character
+		if (strstr(command[i], "|") != NULL) {
+			parse_pipe(command[i]);
+		}
+		if (strstr(command[i], "<") != NULL || strstr(command[i], ">") != NULL) {
+			parse_redir(command[i]);
+		}
+		parse_args(command);
+		if (strcmp(command[0], "exit") == 0) {
+			exit(0);
+		}
+		if (strcmp(command[0], "cd") == 0) {
+			chdir(command[1]);
+			continue;
+		}
+		if (fork() == 0) {
+			execvp(command[0], command);
+		} else {
+			int status;
+			wait(&status);
+		}
+		
+	}
 }
 
+
 void run(){
-  while(1){
-    printf("> ");
-    parse_params();
-  }
+	while(1){
+		printf("Print command: ");
+		parse_params();
+	}
 }
--- shell.h
+++ shell.h
@@ -3,3 +3,5 @@ char** parse_args(char* line);
 int execute_commands(char** args);
 void print_args(char** args);
 int parse_params();
+void parse_pipe(char *command);
+void parse_redir(char *redir);
