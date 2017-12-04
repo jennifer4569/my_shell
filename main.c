f665070454f416d4dffcdaac7e35a4181159821c
Yedoh Kang <ykang@stuy.edu>
2017-12-04 01:26:50 -0500

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,9 @@ int main() {
 	printf("\nNote: MUST use whitespace when using ; or | OR the shell will NOT work properly.\n");
 	printf("Ex: ls ; echo hello\n");
 	printf("Ex: ls | wc \n\n");
+	
 	run();
+	
 	return 0;
 	
 }
